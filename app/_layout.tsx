ab5e5e78e91c52382ffc4da8834ecd247c9ddd9c
PikkiKaju <igor.zmuda02@gmail.com>
2023-11-01 00:14:06 +0100

    Expo, app tree, fonts and images initialized

---
+++ app/_layout.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react';
+
+import { SplashScreen } from 'expo-router';
+import { useFonts } from 'expo-font';
+
+import App from './app';
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from 'expo-router';
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    Comfortaa: require('../assets/fonts/Comfortaa-VariableFont_wght.ttf'),
+  });
+
+  // Catch errors when loading fonts
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  // Hide splash screen after font was loaded
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return <RootLayoutNav />;
+}
+
+function RootLayoutNav() {
+  const [hidden, setHidden] = useState(false);
+
+  return (
+    <>
+      <App />
+    </>
+  );
+}

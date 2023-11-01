75933556fcd51e0acb971eb0ced41ea93f2ff958
PikkiKaju <igor.zmuda02@gmail.com>
2023-11-01 14:36:23 +0100

    Babel and Metro alias configuration

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,22 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['expo-router/babel'],
+    plugins: [
+      'expo-router/babel',
+      [
+        'module-resolver',
+        {
+          root: ["./"],
+          alias: {
+            "@": "./",
+            app: './app',
+            assets: './assets',
+            components: './components',
+            constants: './constants',
+            data: './data',
+          },
+        }
+      ],
+    ],
   };
 };
